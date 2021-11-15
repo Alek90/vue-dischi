a989c2fc45916e5551dd554417dfc0a9133a0a07
Alek90 <4lek.90@gmail.com>
2021-11-15 17:26:08 +0100

    Consegna.
    Modificato font-family, consegna esercizio!

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,7 @@ export default {
 /* importazione bootstrap */
 @import "../node_modules/bootstrap/scss/bootstrap.scss";
 @import "./assets/scss/style.scss";
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap');
 
 
 </style>
