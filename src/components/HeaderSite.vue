0fbba2fbcff7ea988b71f14280b33aa8d379fbbb
Alek90 <4lek.90@gmail.com>
2021-11-16 12:27:10 +0100

    Create music-genre select.
    Creato select per selezionare il genere musicale desiderato.

--- src/components/HeaderSite.vue
+++ src/components/HeaderSite.vue
@@ -3,6 +3,13 @@
 
         <img class="logo" src="https://www.wallpapertip.com/wmimgs/17-176153_download-logo-spotify-png.png" alt="logo Spotify">
 
+        <select name="genere" id="genere">
+            <option value="rock">Rock</option>
+            <option value="pop">Pop</option>
+            <option value="jazz">Jazz</option>
+            <option value="metal">Metal</option>
+        </select>
+
     </header>
 </template>
 
