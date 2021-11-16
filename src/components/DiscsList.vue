2d5d6eef5ee49762692f439c86b050c0a300a3ee
Alek90 <4lek.90@gmail.com>
2021-11-16 16:58:12 +0100

    Complete the request.

--- src/components/DiscsList.vue
+++ src/components/DiscsList.vue
@@ -3,7 +3,7 @@
 
         <div v-if="loading != true" class="discs row mx-4 gy-4 gx-5 justify-content-center">
 
-            <div class="col-md-2 px-4" v-for="disc in discs" :key="disc.title">
+            <div class="col-md-2 px-4" v-for="disc in showFilterDiscs" :key="disc.title">
 
                 <div class="disc p-4 text-center">
 
@@ -34,10 +34,10 @@
 
          <select v-model="choiceGenre" @change="selectChoice()" name="music_genre" id="music_genre">
             <option value="all">All</option>
-            <option value="rock">Rock</option>
-            <option value="pop">Pop</option>
-            <option value="jazz">Jazz</option>
-            <option value="metal">Metal</option>
+            <option value="Rock">Rock</option>
+            <option value="Pop">Pop</option>
+            <option value="Jazz">Jazz</option>
+            <option value="Metal">Metal</option>
         </select>
 
         <!-- quando cambio valore del select, devo filtrare i disc che contengono genre = value. -->
@@ -70,7 +70,7 @@ export default {
         
     },
 
-    methods:{
+    methods: {
         callAPI() {
             axios
             .get('https://flynn.boolean.careers/exercises/api/array/music')
@@ -87,6 +87,21 @@ export default {
             console.log(this.choiceGenre);
         }
 
+    },
+
+    computed: {
+        showFilterDiscs() {
+            if(this.choiceGenre === "all"){
+                console.log('ciao');
+                return this.discs;
+            }
+
+                const filteredDiscs = this.discs.filter((disc) => {
+                    return disc.genre == this.choiceGenre
+                });
+                console.log(filteredDiscs, this.choiceGenre);
+                return filteredDiscs;
+        }
     }
 
     
