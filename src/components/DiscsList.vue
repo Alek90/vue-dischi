bc603cba0afc6cf1765e3941939f30ddc5f67b13
Alek90 <4lek.90@gmail.com>
2021-11-16 12:53:06 +0100

    Move select from header to DiscList.
    Spostato il select per semplificare ragionamenti.

--- src/components/DiscsList.vue
+++ src/components/DiscsList.vue
@@ -32,6 +32,15 @@
 
         </div>
 
+        <select name="music_genre" id="music_genre">
+            <option value="all">All</option>
+            <option value="rock">Rock</option>
+            <option value="pop">Pop</option>
+            <option value="jazz">Jazz</option>
+            <option value="metal">Metal</option>
+        </select>
+
+
     </div>
 
 </template>
--- src/components/HeaderSite.vue
+++ src/components/HeaderSite.vue
@@ -3,14 +3,6 @@
 
         <img class="logo" src="https://www.wallpapertip.com/wmimgs/17-176153_download-logo-spotify-png.png" alt="logo Spotify">
 
-        <select name="music_genre" id="music_genre">
-            <option value="all">All</option>
-            <option value="rock">Rock</option>
-            <option value="pop">Pop</option>
-            <option value="jazz">Jazz</option>
-            <option value="metal">Metal</option>
-        </select>
-
     </header>
 </template>
 
@@ -18,6 +10,9 @@
 export default {
     
     name: 'HeaderSite',
+
+
+
 }
 </script>
 
